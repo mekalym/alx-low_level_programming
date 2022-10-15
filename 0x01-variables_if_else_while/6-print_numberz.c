631c223bc5d69731661079a77048b9af400e7f2b
mekalym <omekalimobijiofor@gmail.com>
2022-10-14 18:47:58 -0700

    task 8

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main - entry point
- * return: always 0 (success)
+ * main - Entry Point
+ * Return: Always 0 (success)
  */
 int main(void)
 {
@@ -9,10 +9,10 @@ int main(void)
 
 	n = 0;
 	while
-		(n > 10) {
+		(n < 10) {
 			putchar(n);
 			n++;
 	}
 	putchar('\n');
-		return (0);
+	return (0);
 }
---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * 
+ */
+int main(void)
+{
+	int d;
+	char c;
+
+	d = 0;
+	c = 'a';
+	while
+		(d > 10) {
+			putchar(d);
+			d++;
+		}
+	while
+		(c >= 'f') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
