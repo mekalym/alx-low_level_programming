97020033a2e0a9c1094e68fa866502ad9e454ef5
mekalym <omekalimobijiofor@gmail.com>
2022-10-14 18:19:47 -0700

    task 6 and 7

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * return: always 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	n = 0;
+	while
+		(n > 10) {
+			putchar(n);
+			n++;
+	}
+	putchar('\n');
+		return (0);
+}
