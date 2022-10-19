a915e266ab2c142482eff6c7b9ccb0ea7042a786
mekalym <omekalimobijiofor@gmail.com>
2022-10-19 13:21:20 -0700

    Task 9-8"

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * times_table - print the 9 times table
+ */
+void times_table(void)
+{
+	int num, mult, prod;
+
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar('0');
+
+		for (mult = 1; mult
+		
