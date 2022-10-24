5544eb61e9d3e832715037d467b9f01e01f5c635
mekalym <omekalimobijiofor@gmail.com>
2022-10-24 16:01:51 -0700

    task 1- 5

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - swap the value of two integers
+ * @a: parameter 1
+ * @b: parameter 2
+ */
+void swap_int(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_rev - printing a string in reverse
+ * @s: the stringb to be printed in rev
+ */
+void print_rev(char *s)
+{
+	int i, n;
+
+	n = 0;
+	while (s[n] != '\0')
+		n++;
+
+	for (i = n - 1; i >= 0; 1--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
