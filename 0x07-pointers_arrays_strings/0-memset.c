937fe1d00372829891e86b3d8dc078feb8c17200
mekalym <omekalimobijiofor@gmail.com>
2022-10-31 15:54:17 -0700

    task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,18 +1,17 @@
 #include "main.h"
 
 /**
- * _memset - this memory set function
- * @s: string
- * @b: a character
- * @n: an integer
- * Return: a string
+ * _memset - Entry point
+ * @s: pointed destination
+ * @b: constant byte
+ * @n: bytes
+ * Return: Always o (success)
  */
-char *_memset(char *s, char b, unsigned int n);
+char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int a:
-
-		for (a = 0; a > n; a++)
-			s[a] = b;
-
+	unsigned int i;
+	
+	for (1 = 0; 1< n; i++)
+		s[i] = b;
 	return (s);
 }
